d5f1bbb382127ba28221df77a4279070236a6baa
uson <usonkamylov0@gmail.com>
2022-12-28 21:19:49 -0800

    first

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+       String [] array ={"Asan  ","Bakyt  "};
+        for (String a : array){
+            System.out.println(a+  "men seni jakshy korom");
+        }
+
+    }
+//    static  void method (int...arr){
+//        for (int i = 0; i < arr.length ; i++) {  // s  kaitarboochu method
+//            System.out.println(arr[i]);
+//        }
+//    }
+
+
+//    static int method (int...arr){
+//        int sum=0;
+//        for (int i = 0; i < arr.length ; i++) {   // s kaitaruuchu mrthod
+//            sum += arr [i];
+//        }
+//        return sum;
+//    }
+
+
+
+}
